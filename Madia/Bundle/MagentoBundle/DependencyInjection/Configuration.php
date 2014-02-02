9a845fc351fdc3ea325e06149c950f31029ccf78
Jaimy Casteleijn <hotlander@gmail.com>
2014-02-02 15:35:35 +0100

    madia magento bundle setup

---
+++ Madia/Bundle/MagentoBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Madia\Bundle\MadiaMagentoBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        /** @var ArrayNodeDefinition $rootNode */
+        $treeBuilder->root('madia_mage');
+       
+        return $treeBuilder;
+    }
+}
---
+++ Madia/Bundle/MagentoBundle/MadiaMagentoBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Madia\Bundle\MadiaMagentoBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MadiaMagentoBundle extends Bundle
+{
+//    public function getParent()
+//    {
+//        return 'OroDashboardBundle';
+//    }
+}
